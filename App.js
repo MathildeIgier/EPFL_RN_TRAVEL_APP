e0307bf95a26eb277c38ab80b94f8d1b84034ba7
MathildeIgier <mathilde.igier@hotmail.com>
2020-03-06 15:20:25 +0100

    Add travel page

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Home from './src/pages/Home/Home';
 import TravelDetails from './src/pages/TravelDetails/TravelDetails';
+import AddTravel from './src/pages/AddTravel/AddTravel';
 
 export default function App() {
     return (
-        <TravelDetails />
+        <AddTravel />
     );
 }
